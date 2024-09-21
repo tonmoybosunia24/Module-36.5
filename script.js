d8033f3fa88c4bd5afab1f521dce7b65dc3ef5a8
tonmoybosunia24 <mdtonmoybosunia24@gmail.com>
2024-09-21 21:08:32 +0600

    Full Module Complete With Practice

---
+++ script.js
@@ -0,0 +1,54 @@
+// const hello = localStorage.getItem("drink", "water")
+// console.log(hello)
+// const setAge = () =>{
+//        localStorage.setItem("phone", "samsung")
+// }
+
+
+
+
+// A simple shopping cart to add product and quantity
+
+
+
+const addproduct = () =>{
+       const productField = document.getElementById("Product-Name")
+       const quantityField = document.getElementById("Product-Quantity")
+       const product = productField.value;
+       const quantity = quantityField.value;
+       productField.value = ""
+       quantityField.value = ""
+       console.log(product, quantity)
+       displayProduct(product, quantity)
+       saveProductToLocalStorage(product, quantity)
+}
+const displayProduct = (product, quantity)=>{
+       const ul = document.getElementById("product-Container")
+       const li = document.createElement("li")
+       li.innerText = `${product} : ${quantity}`
+       ul.appendChild(li);
+}
+const getStoredShoppingCard = () =>{
+       let card = {};
+       const storedCard = localStorage.getItem("card")
+       if(storedCard){
+              card = JSON.parse(storedCard)
+       }
+       return card;
+}
+const saveProductToLocalStorage = (product, quantity) =>{
+       const card = getStoredShoppingCard();
+       card[product] = quantity;
+       const stringFied = JSON.stringify(card)
+       localStorage.setItem("card", stringFied)
+}
+const displayProductsFromLocalStorage = () =>{
+       const savedCard = getStoredShoppingCard();
+       console.log(savedCard)
+       for(product in savedCard){
+              const quantity = savedCard[product]
+              console.log(product, quantity)
+              displayProduct(product, quantity)
+       }
+}
+displayProductsFromLocalStorage()
